efa615686b66b0535c9b7b310450eb6789c37391
xiebaochun <463449055@qq.com>
2015-01-09 00:11:10 +0800

    update

--- template/js/main.js
+++ template/js/main.js
@@ -1,52 +1,63 @@
-$(function () {
-   ///////////////////////////////////////////////////////////window bar click
-                                                                      //窗口栏
-   if(typeof require!="undefined"){
-                                                            
-   
-    
-      var nw = require('nw.gui');
+$(function() {
+  ///////////////////////////////////////////////////////////window bar click
+  //窗口栏
+  if (typeof require != "undefined") {
 
-      var win = nw.Window.get();
 
+
+    var nw = require('nw.gui');
+
+    var win = nw.Window.get();
+
+    win.isMaximized = false;
+
+    win.setMinimumSize(950, 550);
+
+    $("#min").click(function() {
+      win.minimize();
+    });
+
+    $("#max").click(function() {
+      console.log($(window).width() + ":" + screen.width);
+      if ($(window).width() == screen.width) {
+        win.unmaximize();
+      } else {
+        win.maximize();
+      }
+    });
+
+    $("#close").click(function() {
+      win.close();
+    });
+
+
+    win.on('maximize', function() {
+      win.isMaximized = true;
+    });
+
+    win.on('unmaximize', function() {
       win.isMaximized = false;
+    })
 
-      win.setMinimumSize(950, 550);
+    win.on('loaded', function() {
 
-      $("#min").click(function(){
-          win.minimize();
-      });                                                        
-      
-      $("#max").click(function(){
-          if (win.isMaximized){
-              win.unmaximize();
-          }else{
-              win.maximize();
-          }
-      });
-      
-      $("#close").click(function(){
-          win.close();
-      });
 
-      
-      win.on('maximize',function(){
-          win.isMaximized = true;
-      });
+    });
 
-      win.on('unmaximize',function(){
-          win.isMaximized = false;
-      })
-
-      win.on('loaded', function() {
-         
-          
-      });
+    window.onkeydown=function(evt) {
+      if (evt.which == 116) {
+        window.location.reload(true)
+      }
+      if (evt.which == 123) {
+        win.showDevTools();
+      }
     }
-    ////////////////////////////////////////////////////////////your code
-    setTimeout(function(){
-                  $("#loader").fadeOut();  
-                },3000);
-   
-    
+
+  }
+  ////////////////////////////////////////////////////////////your code
+  setTimeout(function() {
+    $("#loader").fadeOut();
+  }, 1000);
+
+
 });
